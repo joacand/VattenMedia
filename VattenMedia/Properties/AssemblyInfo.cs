085acd70aebe3e3ff10c6831222586009e67b074
joacand <tbsitu@gmail.com>
2018-10-28 16:15:35 +0100

    Add AssemblyInfo

--- VattenMedia/Properties/AssemblyInfo.cs
+++ VattenMedia/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VattenMediaWpf")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("VattenMedia")]
+[assembly: AssemblyDescription("A desktop client for watching Twitch streams using a third party streaming utility")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("VattenMediaWpf")]
+[assembly: AssemblyCompany("Joacand")]
+[assembly: AssemblyProduct("VattenMedia")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
